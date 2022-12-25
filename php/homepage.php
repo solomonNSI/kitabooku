bc6fb9ddfe4d3d08e3979cd2f3034436482af842
AtasagunSanap <63560730+AtasagunSanap@users.noreply.github.com>
2022-12-26 01:31:57 +0300

    general design fix falan bişeler

--- php/homepage.php
+++ php/homepage.php
@@ -24,7 +24,7 @@
                     <li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?page=leaderboard">Leaderboard</a></li>
                     <li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?page=buyebook">Buy e-books</a></li>
                     <li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?page=forums">Forums</a></li>
-                    <li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?page=settings">Settings</a></li>
+                    <li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?page=profile">Profile</a></li>
                     <li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?page=publishebook">Publish E-Book</a></li>
                 </ul>
             </div>
@@ -72,8 +72,8 @@
                             include "e-book.php";
                         } elseif ($page == "forums") {
                             include "forums.php";
-                        } elseif ($page == "settings") {
-                            include "settings.php";
+                        } elseif ($page == "profile") {
+                            include "profile.php";
                         } elseif ($page == "allbooks") {
                             include "allbooks.php";
                         } elseif ($page == "publishebook") {
