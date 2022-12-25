e11497d34bd1628402665a9355fae5c30c6ea9ab
Gökberk Keskinkılıç <gkeskinkilic@gmail.com>
2022-12-25 15:27:33 +0300

    login related files, copied from my hw

--- php/login.php
+++ php/login.php
@@ -1,55 +1,22 @@
-<?php
-    include('config.php');
-    $error = "";
-    session_start();
-    if (session_id())
-    {
-        session_destroy();
-    }
-
-    if($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $sql = "SELECT * FROM user WHERE email = '$email' and password = '$password'";
-        $result = mysqli_query($db,$sql);
-        $row = mysqli_num_rows($result);
-        if($row == 1) {
-            session_start();
-            $user = mysqli_fetch_object($result);
-            $u_id = $user->u_id;
-            $userType = $user->user_type;
-            $_SESSION['u_id'] = $u_id;
-            $_SESSION['usertype'] = $usertype;
-            header("location: home.php");
-        }
-        else {
-            $error = "Your Username or Password is invalid";
-        }
-    }
-    ?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html>
+    <head>
+        <meta charset='utf-8'/>
+        <link rel="stylesheet" href="index-style.css">
+        <title>KITABOOKU</title>
+    </head>
 
-<head>
-    <link rel="stylesheet" href="../style.css">
-    <title>Login</title>
-</head>
-
-<body style="background-color:#dddfd4;">
-    <br>
-    <h1 style="text-align:center;" onclick="location.href='http://google.com.tr';" >GLASS CEILING</h1>
-    <br><br><br>
-    <form action="" method="post">
-        <br>
-        <input type="email" class="input" id="email" placeholder="Enter Email" name="email" required>
-        <br>
-        <input type="password" class="input" id="password" placeholder="Enter Password" name="password">
-        <br>
-        <button type="submit" name="input" class="button">Login</button>
-    </form>
-</body>
+    <body>
+    
+    <div id='login'>
+        <h1 class ="title">KITABOOKU</h1>
+        <form class="form" method='post' onsubmit='return validateForm();' action='log.php'>
+        
+        <input type='text' placeholder="Username" id='usernameInput' name='username' required>
+        
+        <input type='password' placeholder="Password" id='passwordInput' name='password' required>
+        <button type='submit'>Login</button>
+        </form>
+    </div id='login'>
+    </body>
 </html>
-<html>
-
