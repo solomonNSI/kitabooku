80d58dfd09bffc4adc2b84e4a133a2d7864cb3c7
AtasagunSanap <63560730+AtasagunSanap@users.noreply.github.com>
2022-12-27 00:10:33 +0300

    range search added

--- php/allbooks.php
+++ php/allbooks.php
@@ -33,30 +33,41 @@
         <tr>
             <th>Book Title</th>
             <th>Author</th>
+            <th>Page</th>
         </tr>
         <!-- All books table -->
         <h1> All Books here </h1>
         <?php
 
-        if (!$db) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        if (isset($_POST["filter"])) {
+       
+        if (isset($_POST["name"]) || isset($_POST["min_page"]) || isset($_POST["max_page"])) {
 
             session_start();
             require 'ConnectServer.class.php';
             $db = ConnectServer::connect();
-            $value = $_POST["filter"];
-            echo "<h4> Some weird php error that we couldn't solve, but your input was: " . $value . "</h4>";
-            $sql = "SELECT * FROM Book
-                        WHERE title LIKE '%" . $value . "%'";
-
-            echo $sql;
+            $name = $_POST["name"];
+            $min_page = $_POST["min_page"];
+            $max_page = $_POST["max_page"];
+            if ($name != "") {
+                $sql = "SELECT * FROM Book
+                WHERE title LIKE '%" . $name . "%'";
+            } elseif ($min_page != "" && $max_page != "") {
+                $sql = "SELECT * FROM Book
+                WHERE page_count BETWEEN " . $min_page . " AND " . $max_page;
+            } elseif ($min_page != "") {
+                $sql = "SELECT * FROM Book
+                WHERE page_count >= " . $min_page;
+            } elseif ($max_page != "") {
+                $sql = "SELECT * FROM Book
+                WHERE page_count <= " . $max_page;
+            } else {
+                $sql = "SELECT * FROM Book";
+            }
             $result = $db->query($sql) or die('<script>alert("Account already exists");');
             if (mysqli_num_rows($result) > 0) {
                 echo "<h4> Found: " . mysqli_num_rows($result) . " books </h4>";
                 while ($row = mysqli_fetch_array($result)) {
-                    echo "<tr><td><a href='bookview.php?b_id=" . $row['b_id'] . "'>" . $row['title'] . "</a></td><td>" . $row['author'] .  "</td></tr>";
+                    echo "<tr><td><a href='bookview.php?b_id=" . $row['b_id'] . "'>" . $row['title'] . "</a></td><td>" . $row['author'] . "</td><td>" . $row['page_count'] .  "</td></tr>";
                 }
             } else {
                 echo "<tr><td>Nothing Found</td></tr>";
@@ -73,7 +84,7 @@
                 while ($row = mysqli_fetch_assoc($result)) {
                     // Create a clickable link using the title as the anchor text
                     // Pass the title as a parameter in the URL using the "title" query string variable
-                    echo "<tr><td><a href='bookview.php?b_id=" . $row['b_id'] . "'>" . $row['title'] . "</a></td><td>" . $row['author'] . "</td></tr>";
+                    echo "<tr><td><a href='bookview.php?b_id=" . $row['b_id'] . "'>" . $row['title'] . "</a></td><td>" . $row['author'] . "</td><td>" . $row['page_count'] .  "</td></tr>";
                 }
             } else {
                 echo "No reviews found.";
@@ -84,10 +95,16 @@
 </body>
 <div id='applyfilter'>
     <h1>Apply a filter! </h1>
-    <form class="form" method='post' action='e-book.php'>
-        <label for="filter"> Search for the name (ex. first two letters) </label>
-        <input type="text" name="filter" id="filter"> <br>
-        <button type="submit">Search!</button>
+    <form class="form" method='post' action='allbooks.php'>
+        <label for="name"> Search for the name (ex. first two letters) </label>
+        <input type="text" name="name" id="name"> <br>
+        <label for="min_page">Minimum number of pages:</label>
+        <input type="number" id="min_page" name="min_page">
+        <br>
+        <label for="max_page">Maximum number of pages:</label>
+        <input type="number" id="max_page" name="max_page">
+        <br>
+        <input type="submit" value="Search">
     </form>
 </div id='applyfilter'>
 <h1> Couldn't find a book?</h1>
